b3deb695db9b206e4bf4c1df5bdda4aa7765543d
KYH04444 <kyh990312@gmail.com>
2024-11-05 17:05:26 +0900

    commit

---
+++ include/types.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+
+template <typename scalar>
+struct ImuData {
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    double timestamp;
+    Eigen::Matrix<scalar, 3, 1> acc;
+    Eigen::Matrix<scalar, 3, 1> gyr;
+};
+
+template <typename scalar>
+struct UwbData {
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    double timestamp;
+    Eigen::Matrix<scalar, 8, 1> distance;
+};
+
+template <typename scalar>
+struct StateforEKF{
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    Eigen::Matrix<scalar, 3, 1> p;
+    Eigen::Matrix<scalar, 3, 3> R;
+    Eigen::Matrix<scalar, 3, 1> v;
+    Eigen::Matrix<scalar, 3, 1> a_b;
+    Eigen::Matrix<scalar, 3, 1> w_b;
+};
+
+template <typename scalar>
+struct StateforESKF{
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    Eigen::Matrix<scalar, 3, 1> p;
+    Eigen::Quaterniond q; //quaternion w, x, y, z 
+    Eigen::Matrix<scalar, 3, 1> v;
+    Eigen::Matrix<scalar, 3, 1> a_b;
+    Eigen::Matrix<scalar, 3, 1> w_b;
+};
